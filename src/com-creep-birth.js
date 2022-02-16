05410176dc025d71ae8ac5e57698b83b27006601
HEX <1322423005@qq.com>
2022-02-16 18:03:40 +0800

    可以使用
    无bug但是健壮性差耦合度高

---
+++ src/com-creep-birth.js
@@ -0,0 +1,21 @@
+var creepsBirth = {
+
+	/**  *
+	 * @param roleName 存到内存中的role名
+	 * @param limitNum 限制数量
+	 * @param body 携带部件
+	 */
+	run: function (roleName, limitNum, body) {
+		let creepsNum = _.filter(Game.creeps, (creep) => creep.memory.role == roleName);
+		if (creepsNum.length < limitNum) {// 没有到规定数量
+			var newName = roleName + Game.time;
+			let ex = Game.spawns['Spawn1'].spawnCreep(body, newName, {memory: {role: roleName}})
+			// console.log(roleName + "创建" + ex)
+			return false;
+		}
+		console.log(roleName + "到了规定数量")
+		return true;
+	}
+};
+
+module.exports = creepsBirth;
